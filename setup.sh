f6e184c6d4a63125561c377805b2ab383d169556
khalidhub7 <louguikhalid7@gmail.com>
2026-01-17 19:36:51 +0100

    fix build mode

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 
 prettier --print-width 80 --write public/index.html
 
-
 tree -I "node_modules|package-lock.json|setup.sh|*.png|*.jpg|*.jpeg" -F > structure.txt
+
+rsync -av --filter=':- .gitignore' ./ ~/github/streamer-links/
